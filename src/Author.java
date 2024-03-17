cb637b61c0e48564d869eb874832c9143cb0258b
Maciej Miarka <mmiarka.pl@gmail.com>
2024-03-17 14:13:28 +0100

    Obiektowosc

--- src/Author.java
+++ src/Author.java
@@ -10,4 +10,12 @@ public class Author {
     public void setName_author(String name_author) {
         this.name_author = name_author;
     }
+
+    public int getYear_author() {
+        return year_author;
+    }
+
+    public void setYear_author(int year_author) {
+        this.year_author = year_author;
+    }
 }
